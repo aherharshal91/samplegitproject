64afaa6063aa19016f7f3699ae6b3948818bf4bf
harshal <aherharshal91@gmail.com>
2026-04-09 11:56:10 +0530

    new code add into file

--- src/main/java/basicPart1/program1.java
+++ src/main/java/basicPart1/program1.java
@@ -3,7 +3,16 @@ package basicPart1;
 public class program1 {
     public static void main (String[] args)
     {
-        System.out.print("Hello this a First Program");
-        System.out.print("new changes 00");
+        System.out.println("Hello this a First Program");
+        System.out.println("new changes 00");
+
+        for (int i=0;i<5;i++)
+        {
+            for (int j=1;j<i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
