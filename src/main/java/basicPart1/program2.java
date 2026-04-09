6dbbf968963f45dc810e51cee1ab9b22a945d317
harshal <aherharshal91@gmail.com>
2026-04-09 14:51:58 +0530

    new reverse program added

---
+++ src/main/java/basicPart1/program2.java
@@ -0,0 +1,15 @@
+package basicPart1;
+
+public class program2 {
+    public static void main (String [] args)
+    {
+        String a = "Harshal Aher";
+        String rev = "";
+        for (int i= a.length()-1;i>=0;i--)
+        {
+            rev= rev + a.charAt(i);
+
+            System.out.println(rev);
+        }
+    }
+}
