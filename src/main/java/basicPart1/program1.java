14139bbb85da02d9bc763eb6534caf69b9e0aaf3
harshal <aherharshal91@gmail.com>
2026-04-09 11:43:16 +0530

    new data added into file

--- src/main/java/basicPart1/program1.java
+++ src/main/java/basicPart1/program1.java
@@ -4,5 +4,6 @@ public class program1 {
     public static void main (String[] args)
     {
         System.out.print("Hello this a First Program");
+        System.out.print("new changes 00");
     }
 }
