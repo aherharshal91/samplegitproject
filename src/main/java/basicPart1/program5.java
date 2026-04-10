18dfbb34e1b6192cc42febfe454f05d2a492e351
harshal <aherharshal91@gmail.com>
2026-04-10 16:41:06 +0530

    program 5 add in master

---
+++ src/main/java/basicPart1/program5.java
@@ -0,0 +1,33 @@
+package basicPart1;
+
+public class program5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        //using for loop print below pattern
+		        /*
+		         *	*	*	*
+		         *	*	*
+		         *	*
+		         *
+		         */
+
+		        for (int i=1;i<5;i++) {
+		            for (int j = 1; j <= 5 - i; j++) {
+
+		                System.out.print("*");
+		                System.out.print("\t");
+
+
+		            }
+		            System.out.println();
+		        }
+
+		        }
+
+
+
+		    }
+
+
