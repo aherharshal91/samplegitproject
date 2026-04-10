b24d4d1168e3ec2c9d5c31243b24c3f6c500db81
harshal <aherharshal91@gmail.com>
2026-04-10 16:35:33 +0530

    program 4 add in master

---
+++ src/main/java/basicPart1/program4.java
@@ -0,0 +1,11 @@
+package basicPart1;
+
+public class program4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("check the status");
+
+	}
+
+}
